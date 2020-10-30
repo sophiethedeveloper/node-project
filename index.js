bdaec7ad8f0368f0d1e37a06864d0cb37f576281
sofia Luna <Sofia21luna@gmail.com>
2020-10-29 19:14:23 -0600

    calling all the packapages

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require('express') // imports express
 require('dotenv').config()
+const express = require('express') // imports express
+const helmet = require('helmet')
+const morgan = require('morgan')
+const cors = require('cors')
+require('colors')
 
 const server = express() /// allows our app to use express
 
+server.use(helmet())
+server.use(morgan('dev'))
+server.use(cors())
+server.use(express.json())
+
 const PORT = process.env.PORT || 5000;
 
+server.get('/', (req, res) => {
+    res.send('Endpoint Running!')
+})
+
 server.listen(PORT, () => {
-    console.log(`\n** Server listening on port ${PORT}`) // listens for a port
+    console.log(`\n** Server listening on port ${PORT}`.america) // listens for a port
 })
